503c3f164f94be02ff341a9ce098c95189047b7a
RaStm <rasimgokmen@gmail.com>
2025-05-29 23:09:11 +0300

    v2

--- Libs/Remote.hpp
+++ Libs/Remote.hpp
@@ -2,9 +2,10 @@
 #include "stm32f4xx.h"
 
 struct PPM{
-	bool first_rising_edge = false;
+//	bool first_rising_edge = false;
+	bool first_valid_edge  = false;
 	bool dead_space_seen   = false;
-	int16_t period = 0;
-	int16_t channelX[8] = {0};
+	uint16_t last_time = 0, current_time = 0, delta = 0;
+	int32_t channelX[8] = {0};
 	int8_t  i=0;
 };
