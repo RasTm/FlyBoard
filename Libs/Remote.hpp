8821da9035dcfee2b490a2059776da125114ccec
RaStm <rasimgokmen@gmail.com>
2025-06-02 00:21:14 +0300

    add new ppm failsafe feature

--- Libs/Remote.hpp
+++ Libs/Remote.hpp
@@ -2,10 +2,10 @@
 #include "stm32f4xx.h"
 
 struct PPM{
-//	bool first_rising_edge = false;
 	bool first_valid_edge  = false;
 	bool dead_space_seen   = false;
 	uint16_t last_time = 0, current_time = 0, delta = 0;
 	int32_t channelX[8] = {0};
 	int8_t  i=0;
+	uint8_t remote_frame_lost_counter = 0;
 };
